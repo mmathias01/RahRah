a66d499a1112728d80b8abb73cf1c784c2dd3a23
Matthew Mathias <matthew.mathias@me.com>
2016-10-17 18:44:12 -0600

    none

--- js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('h1 a', { fontFamily: 'Scriptina', textShadow:"1px 1px rgba(255,255,255,.4),-1px -1px rgba(255,255,255,.4)" });
-Cufon.replace('h1 .desc', { fontFamily: 'Century Gothic' });
-Cufon.replace('h2,a.more',{fontFamily:'Walkway Bold',textShadow:'1px 1px #000',hover:{}})
